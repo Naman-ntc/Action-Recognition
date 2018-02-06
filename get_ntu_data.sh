012d99c8c9189d5ea9513488c704778327ffa148
Naman Jain <naman1205jain@gmail.com>
2018-02-06 14:14:16 +0000

    Perfected NTU script :D

--- get_ntu_data.sh
+++ get_ntu_data.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 clear
-mkdir datasets
+mkdir -v datasets
 cd datasets
-mkdir NTU
+mkdir -v NTU
 cd NTU
+echo "Downloading the nturgbd_skeletons zip file"
 wget http://rose1.ntu.edu.sg/Datasets/actionRecognition/download/nturgbd_skeletons.zip
 sudo apt-get install unzip
 unzip nturgbd_skeletons.zip
+rm nturgbd_skeletons.zip
+echo "Downloading dependencies from github"
 wget https://raw.githubusercontent.com/yysijie/st-gcn/master/tools/ntu_read_skeleton.py
 wget https://raw.githubusercontent.com/yysijie/st-gcn/master/tools/ntu_gendata.py
+wget https://raw.githubusercontent.com/shahroudy/NTURGB-D/master/Matlab/samples_with_missing_skeletons.txt
+echo "IMPORTANT"
+echo "Make changes of line 78 of the program, change w to wb"
+nano ntu_gendata.py
+echo "Now pickling time!!"
