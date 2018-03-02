6c0a97046e1801eeee38716b2ddb4c8a39ae5c45
Sahil Shah <sahilshah00199@gmail.com>
2018-03-03 00:44:53 +0530

    finished up helper functions

--- Sahil/helperFunctions.py
+++ Sahil/helperFunctions.py
@@ -2,9 +2,25 @@ import pickle
 import numpy as np 
 
 def getData():
-	model = 
+	model = np.load('../datasets/NTU/xsub/Final-Data/train_data.npy')
+	model = model[:300,:,:,:]
 	return model
 
 def getLabels():
-	labels = 
+	model = np.load('../datasets/NTU/xsub/Final-Data/train_labels.npy')
+	labels = labels[:300]
 	return labels
+
+def getValData():
+	model = np.load('../datasets/NTU/xsub/Final-Data/val_data.npy')
+	model = model[:300,:,:,:]
+	return model
+
+def getValLabels():
+	model = np.load('../datasets/NTU/xsub/Final-Data/val_labels.npy')
+	labels = labels[:300]
+	return labels
+
+def checkAcc(model, data, labels):
+	pred = model(data)
+	return np.mean(pred == labels)
