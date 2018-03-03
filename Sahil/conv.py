133eba2a0aa48f0568e60ba0d126f29cac3efc4f
Naman <naman1205jain@gmail.com>
2018-03-03 10:01:03 +0000

    le merge kr :P

--- Sahil/conv.py
+++ Sahil/conv.py
@@ -5,6 +5,35 @@ from keras.optimizers import SGD, RMSprop, Adam #change to adam
 from keras.utils import to_categorical
 from helperFunctions import *
 
+
+class PlotLosses(keras.callbacks.Callback):	
+	def on_train_begin(self, logs={}):
+		self.i = 0
+		self.x = []
+		self.losses = []
+		self.val_losses = []
+		
+		self.fig = plt.figure()
+		
+		self.logs = []
+
+	def on_batch_end(self, epoch, logs={}):
+		
+		self.logs.append(logs)
+		self.x.append(self.i)
+		self.losses.append(logs.get('loss'))
+		self.i += 1
+		
+		#clear_output(wait=True)
+	
+	def on_epoch_end(self, epoch, logs={}):
+		plt.plot(self.x, self.losses, label="loss")
+		plt.legend()
+		plt.show()
+		plt.savefig('current_loss.png')
+		plt.close()
+		TrainAcc()	
+
 learningRate = 1e-3
 batchSize = 64
 
@@ -41,7 +70,8 @@ print(model.summary())
 #print(checkAcc(model,trainingData,labels))
 
 rmsprop = RMSprop(lr=learningRate, rho=0.9, epsilon=None, decay=0.0)
-model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(loss='categorical_crossentropy', optimizer=adam)
 
 plot_losses = PlotLosses()
 
@@ -63,3 +93,4 @@ def ValAcc():
 	labels = getValLabels()
 	labels = to_categorical(labels,num_classes=49)
 	print(checkAcc(model,trainingData,labels))	
+
