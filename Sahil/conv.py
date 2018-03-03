89efe4b95c9155c1803bc9a0da985f001df30403
Naman <naman1205jain@gmail.com>
2018-03-03 09:25:27 +0000

    started training

--- Sahil/conv.py
+++ Sahil/conv.py
@@ -1,30 +1,65 @@
 from keras import Sequential
-from keras.layers import Dense, Conv2D, Activation, MaxPooling2D
-from keras.layers import GLobalAveragePooling2D
+from keras.layers import Dense, Conv2D, Activation, MaxPooling2D, BatchNormalization
+from keras.layers import GlobalAveragePooling2D
 from keras.optimizers import SGD, RMSprop, Adam #change to adam
-
+from keras.utils import to_categorical
 from helperFunctions import *
 
 learningRate = 1e-3
-batchSize = 32
+batchSize = 64
 
 trainingData = getData()
 labels = getLabels()
+labels = to_categorical(labels,num_classes=49)
 
 model = Sequential()
-model.add(Conv2D(32, (3,3), padding=(1,1), activation='relu', input_shape=(1, None, None)))
-model.add(Conv2D(32, (3,3), padding=(1,1), activation='relu'))
-model.add(Conv2D(64, (3,3), padding=(1,1), activation='relu'))
+model.add(BatchNormalization(input_shape=(300,25,3)))
+model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size = (2,2)))
-model.add(Conv2D(64, (3,3), padding=(1,1), activation='relu'))
-model.add(Conv2D(64, (3,3), padding=(1,1), activation='relu'))
-model.add(Conv2D(128, (3,3), padding=(1,1), activation='relu'))
-model.add(Conv2D(128, (3,3), padding=(1,1), activation='relu'))
-model.add(GLobalAveragePooling2D(data_format-'channels_first'))
-model.add(Dense(128), activation='relu')
-model.add(Dense(64), activation='relu')
-model.add(Dense(48), activation='softmax')
+model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(GlobalAveragePooling2D())
+model.add(Dense(128, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(64, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(49, activation='softmax'))
 
-rmsprop = keras.optimizers.RMSprop(lr=learningRate, rho=0.9, epsilon=None, decay=0.0)
+print(model.summary())
+
+#print(checkAcc(model,trainingData,labels))
+
+rmsprop = RMSprop(lr=learningRate, rho=0.9, epsilon=None, decay=0.0)
 model.compile(loss='categorical_crossentropy', optimizer=rmsprop)
-model.fit(trainingData, labels, batch_size = batchSize, epochs = 10)
+
+plot_losses = PlotLosses()
+
+# c.model.fit(c.trainingData, c.labels, batch_size = c.batchSize, epochs = 100)
+# c.checkAcc(c.model,c.trainingData,c.labels)
+
+def train(batch_size=64,epochs=6):
+	model.fit(trainingData, labels, batch_size = batchSize, epochs = epochs, callbacks=[plot_losses])
+	TrainAcc()
+
+def change_lr(new_lr):
+	rmsprop.lr = new_lr
+
+def TrainAcc():
+	print(checkAcc(model,trainingData,labels))
+
+def ValAcc():
+	trainingData = getValData()
+	labels = getValLabels()
+	labels = to_categorical(labels,num_classes=49)
+	print(checkAcc(model,trainingData,labels))	
--- Sahil/helperFunctions.py
+++ Sahil/helperFunctions.py
@@ -1,26 +1,60 @@
+import matplotlib
+matplotlib.use('Agg')
 import pickle
 import numpy as np 
+import matplotlib.pyplot as plt
+import keras
 
 def getData():
 	model = np.load('../datasets/NTU/xsub/Final-Data/train_data.npy')
-	model = model[:300,:,:,:]
+	model = np.swapaxes(np.swapaxes(model[:,:,:,:],1,3),1,2)
 	return model
 
 def getLabels():
 	model = np.load('../datasets/NTU/xsub/Final-Data/train_labels.npy')
-	labels = labels[:300]
+	labels = model
+	labels = labels.reshape(-1,1)
 	return labels
 
 def getValData():
 	model = np.load('../datasets/NTU/xsub/Final-Data/val_data.npy')
-	model = model[:300,:,:,:]
+	model = np.swapaxes(np.swapaxes(model[:,:,:,:],1,3),1,2)
 	return model
 
 def getValLabels():
 	model = np.load('../datasets/NTU/xsub/Final-Data/val_labels.npy')
-	labels = labels[:300]
+	labels = model
+	labels = labels.reshape(-1,1)
 	return labels
 
 def checkAcc(model, data, labels):
-	pred = model(data)
-	return np.mean(pred == labels)
+	pred = model.predict(data)
+	return np.mean(np.argmax(pred,axis=1) == np.argmax(labels,axis=1))
+
+class PlotLosses(keras.callbacks.Callback):
+	def on_train_begin(self, logs={}):
+		self.i = 0
+		self.x = []
+		self.losses = []
+		self.val_losses = []
+		
+		self.fig = plt.figure()
+		
+		self.logs = []
+
+	def on_batch_end(self, epoch, logs={}):
+		
+		self.logs.append(logs)
+		self.x.append(self.i)
+		self.losses.append(logs.get('loss'))
+		self.i += 1
+		
+		#clear_output(wait=True)
+	
+	def on_epoch_end(self, epoch, logs={}):
+		plt.plot(self.x, self.losses, label="loss")
+		plt.legend()
+		plt.show()
+		plt.savefig('current_loss.png')
+		plt.close()
+		
