2ae5a0f0a5407a147b1b4a0d95cc3e1ab0423265
Naman Jain <naman1205jain@gmail.com>
2018-03-05 16:09:31 +0530

    enabled cuda

--- Naman/LSTM.py
+++ Naman/LSTM.py
@@ -5,6 +5,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 from helperFunctions import *
 
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+
 def to_categorical(y, num_classes):
     """ 1-hot encodes a tensor """
     return np.eye(num_classes, dtype='uint8')[y]
