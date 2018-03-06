63e5c42378ba1a4c4cd9a06525482f0ee1e83429
Naman <naman1205jain@gmail.com>
2018-03-06 05:51:47 +0000

    added Scheduler support

--- Naman/LSTM.py
+++ Naman/LSTM.py
@@ -22,7 +22,7 @@ class LSTMClassifier(nn.Module):
 	def __init__(self, hidden_dim=64,label_size=49,modified_input_dim=64):
 		super(LSTMClassifier, self).__init__()
 		self.hidden_dim = hidden_dim
-		self.fully_connected = nn.Sequential(nn.Linear(75, 70),nn.ReLU(),nn.Linear(70, 64),nn.ReLU(),nn.Linear(64, 64),nn.ReLU())
+		self.fully_connected = nn.Sequential(nn.Linear(75, 70),nn.ReLU(),nn.Linear(70, 64),nn.ReLU())
 		self.lstm = nn.LSTM(modified_input_dim, hidden_dim)
 		self.hidden2label = nn.Linear(hidden_dim, label_size)
 		self.hidden = self.init_hidden()
@@ -69,7 +69,7 @@ trainingData = neededData
 def checkAcc(data,labels):
 	l = labels.size()[0]
 	labelsdash = autograd.Variable(labels.view(l))
-	l = 100
+	l = 1000
 	out_labels = autograd.Variable(torch.zeros(l))
 	for i in range(l):
 		temp = model0(autograd.Variable(trainingData[i,:,:,:].view(300,1,75)))
@@ -86,8 +86,8 @@ def TrainAcc():
 
 
 #print(labels.size())
-def train(model, num_epoch, num_iter, rec_interval, disp_interval):
-	optimizer = optim.Adam(model.parameters(), lr = 1e-3)
+def train(model, num_epoch, num_iter, lr=1e-3,rec_interval=2, disp_interval=10):
+	optimizer = optim.Adam(model.parameters(), lr)
 	loss_values = []
 	rec_step = 0
 	for eph in range(num_epoch):
@@ -107,7 +107,7 @@ def train(model, num_epoch, num_iter, rec_interval, disp_interval):
 			loss = F.cross_entropy(y_hat, Y)
 			avg_loss += loss.data[0]
 			if i % disp_interval == 0:
-				print('epoch: %d iterations: %d loss :%g\n' % (eph, i, loss.data[0]))
+				print('epoch: %d iterations: %d loss :%g' % (eph, i, loss.data[0]))
 			if rec_step%rec_interval==0:
 				loss_values.append(loss.data[0])
 			model.hidden = (model.hidden[0].detach(), model.hidden[1].detach())
@@ -124,8 +124,19 @@ def train(model, num_epoch, num_iter, rec_interval, disp_interval):
 
 #l = train(model0, 10, 100, 2, 20)
 
-def PlotLoss(l):
+def PlotLoss(l,name):
 	plt.plot(l)			
 	plt.show()
-	plt.savefig('loss.png')
+	plt.savefig(name)
 
+def Scheduler():
+	loss0 = train(model0,3,3300,6e-3)
+	loss1 = train(model0,40,3300,1e-3)
+	PlotLoss(loss1,'loss1.png')
+	TrainAcc()
+	loss2 = train(model0,40,3300,1e-4)
+	PlotLoss(loss1+loss2,'loss2.png')
+	TrainAcc()
+	loss3 = train(model0,50,3300,1e-5)
+	PlotLoss(loss1+loss2+loss3,'loss3.png')
+	TrainAcc()
