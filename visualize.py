7df3c8e2f8c7ee15d58faca43661db5f2f240d52
Naman Jain <naman1205jain@gmail.com>
2018-03-09 12:04:05 +0530

    visualize script

--- visualize.py
+++ visualize.py
@@ -5,9 +5,9 @@ import time
 a = np.load('temp.npy')
 
 for i in range(300):
-	plt.plot(a[3,0,i,:],a[3,1,i,:],'ro', alpha = 0.5)
+	plt.plot(a[5,0,i,:],a[5,1,i,:],'ro', alpha = 0.5)
 	for j in range(25):
-		plt.text(a[3,0,i,j],a[3,1,i,j], str(j))
+		plt.text(a[5,0,i,j],a[5,1,i,j], str(j))
 	plt.show()
 	#time.sleep(0.2)
 	print("Frame%d"%(i))
