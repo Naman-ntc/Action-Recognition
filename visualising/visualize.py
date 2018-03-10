85fa6f0f9403ed8926626279b5491c6a1338cc93
Sahil Shah <sahilshah00199@gmail.com>
2018-03-11 02:23:49 +0530

    Sorted shit out

--- visualising/visualize.py
+++ visualising/visualize.py
@@ -1,13 +1,35 @@
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-a = np.load('temp.npy')
+a = np.load('toyData/valData.npy')
+
+# for i in range(300):
+# 	plt.figure()
+# 	plt.plot(a[5,0,i,:],a[5,1,i,:],'ro', alpha = 0.5)
+# 	for j in range(25):
+# 		plt.text(a[5,0,i,j],a[5,1,i,j], str(j))
+# 	plt.draw()
+# 	plt.show()
+# 	#time.sleep(0.002)
+# 	#plt.clf()
+	
+# 	print("Frame%d"%(i))
+
+
+
+index = 50
 
 for i in range(300):
-	plt.plot(a[5,0,i,:],a[5,1,i,:],'ro', alpha = 0.5)
-	for j in range(25):
-		plt.text(a[5,0,i,j],a[5,1,i,j], str(j))
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	xs = a[index, 0, i, :]
+	ys = a[index, 1, i, :]
+	zs = a[index, 2, i, :]
+	#for j in range(25):
+		#plt.text(a[5,0,i,j],a[5,1,i,j], a[5,2,i,j], str(j)) 	
+	ax.scatter(xs, ys, zs, c = 'r', marker = 'o')
 	plt.show()
-	#time.sleep(0.2)
+
 	print("Frame%d"%(i))
