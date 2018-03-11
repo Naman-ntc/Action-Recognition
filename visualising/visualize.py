5bb7228df4be7e5173e82ace3121c93c1204ae1d
Naman Jain <naman1205jain@gmail.com>
2018-03-11 22:57:38 +0530

    edited visualize

--- visualising/visualize.py
+++ visualising/visualize.py
@@ -29,7 +29,11 @@ for i in range(300):
 	zs = a[index, 2, i, :]
 	#for j in range(25):
 		#plt.text(a[5,0,i,j],a[5,1,i,j], a[5,2,i,j], str(j)) 	
-	ax.scatter(xs, ys, zs, c = 'r', marker = 'o')
+	ax.scatter(xs, ys, zs, c = 'r', marker = 'o',alpha=0.5)
+	for i in range(25):
+		ax.annotate(i,(xs[i],ys[i],zs[i]))
+	## If doesnt work check https://stackoverflow.com/questions/14432557/matplotlib-scatter-plot-with-different-text-at-each-data-point
+	## hopefully it works!!	
 	plt.show()
 
 	print("Frame%d"%(i))
