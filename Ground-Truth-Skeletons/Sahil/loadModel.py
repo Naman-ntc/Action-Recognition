bcb061f2a52d4bce1e3f89262dcf6062a0dddca8
Naman Jain <naman1205jain@gmail.com>
2018-05-05 14:13:41 +0530

    removing

--- Ground-Truth-Skeletons/Sahil/loadModel.py
+++
@@ -1,7 +0,0 @@
-import twoLSTMcuda as t
-model = t.torch.load(open("twoLSTMentireModel.npy", 'rb'))
-print(t.checkAcc(model, t.data, t.labels))
-print(t.checkAcc(model, t.valData, t.valLabels))
-
-
-
